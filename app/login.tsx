549fcc68b112785276b0432635c4362fc01e7ebb
RuanGoms <ruangomes.back@gmail.com>
2025-01-26 13:18:23 +0000

    feat: change login function name

--- app/login.tsx
+++ app/login.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import LoginScreen from '@/screens/Login/LoginScreen';
 
-export default function LoginTab() {
+export default function Login() {
   return (
     <>
       <StatusBar style="auto" />
